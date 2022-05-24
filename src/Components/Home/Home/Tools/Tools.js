6c706dc5ea82bba6fd542ef035486bd7e783f535
Waheedul Islam <waheeduli81@gmail.com>
2022-05-24 14:32:00 +0600

    tools

--- src/Components/Home/Home/Tools/Tools.js
+++ src/Components/Home/Home/Tools/Tools.js
@@ -13,13 +13,15 @@ const Tools = () => {
             })
     }, [])
     return (
-        <div>
-            <h1>This is tools: {items.length}</h1>
-            {
-                items.map(item => <PerTools
-                    item={item}
-                ></PerTools>)
-            }
+        <div className='my-12'>
+            <h1 className='text-center text-2xl font-bold text-primary'>Tools: {items.length}</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    items.map(item => <PerTools
+                        item={item}
+                    ></PerTools>)
+                }
+            </div>
         </div>
     );
 };
