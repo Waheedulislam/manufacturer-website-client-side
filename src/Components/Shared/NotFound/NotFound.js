e85696cf5e5b935dfa78eef1cb3f55c0427ae726
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-25 22:50:09 +0600

    notFound

--- src/Components/Shared/NotFound/NotFound.js
+++ src/Components/Shared/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Not from '../../../img/social/404.png'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1 className='text-2xl text-error'>404 Not Found</h1>
+        <div className='flex justify-center items-center pt-4'>
+            <img src={Not} alt="" />
         </div>
     );
 };
