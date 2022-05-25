f27748bf3387f426011622dbfd4cb58c7cb9a748
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-25 19:48:27 +0600

    commit

--- src/Components/Purchase/Purchase.js
+++ src/Components/Purchase/Purchase.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Purchase = () => {
     const { purchaseId } = useParams();
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const url = `data.json${purchaseId}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setItems(data)
+            })
+    }, [])
     return (
         <div>
             <h1 className='text-4xl'>this is Purchase  page : {purchaseId}</h1>
+            <h1>{items.name}</h1>
         </div>
     );
 };
