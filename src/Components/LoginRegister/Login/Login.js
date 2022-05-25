b3eaba6440ffffdc565ed9fa0c88bd6a2a3bb011
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-25 16:23:05 +0600

    requreAuth-done

--- src/Components/LoginRegister/Login/Login.js
+++ src/Components/LoginRegister/Login/Login.js
@@ -3,7 +3,7 @@ import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-fireba
 import auth from '../../../firebase.init'
 import { useForm } from "react-hook-form";
 import Loading from '../../Shared/Loading/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -17,6 +17,9 @@ const Login = () => {
     ] = useSignInWithEmailAndPassword(auth);
 
     let signInError;
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
 
     if (error || gError) {
         signInError = <h5 className=' text-red-500 pt-0 pb-2'><small>{error?.message ||
@@ -28,7 +31,7 @@ const Login = () => {
     }
 
     if (user || gUser) {
-        console.log(gUser);
+        navigate(from, { replace: true });
     }
 
 
