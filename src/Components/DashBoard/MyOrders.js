ae502497bff17414462bf6f5148b5fee7544aba9
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-26 18:20:02 +0600

    ok

--- src/Components/DashBoard/MyOrders.js
+++ src/Components/DashBoard/MyOrders.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    console.log(orders);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/itemOrder/${user?.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data));
+        }
+    }, [user]);
+
     return (
         <div>
-            <h1>My Orders</h1>
+            <h1>My Orders: {orders.length}</h1>
         </div>
     );
 };
