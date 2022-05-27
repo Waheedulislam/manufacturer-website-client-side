d19bb4962f3f29e3e6c9121c471eefaf4e55609b
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-27 09:56:02 +0600

    admin

--- src/Components/DashBoard/UserRow.js
+++ src/Components/DashBoard/UserRow.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const UserRow = ({ user }) => {
-    const { email } = user;
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')} `
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch();
+                toast.success(`Successfully Make a admin`)
+            })
+    }
     return (
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td><button className="btn btn-xs btn-primary">Make Admin</button></td>
+            <td>{role !== 'admin' && <button className="btn btn-xs btn-primary" onClick={makeAdmin}>Make Admin</button>}</td>
             <td><button className="btn btn-xs btn-error">Remove User</button></td>
-        </tr>
+            <ToastContainer />
+        </tr >
     );
 };
 
