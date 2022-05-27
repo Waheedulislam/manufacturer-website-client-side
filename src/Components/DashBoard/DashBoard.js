e532e2bb2b912bdf4fc83dfda30a7abbce02a90f
Waheedul Islam <waheeduli81@gmail.com>
2022-05-27 16:42:03 +0600

    addReview

--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const DashBoard = () => {
+    const [user] = useAuthState(auth)
+    const [admin] = useAdmin(user);
+
     return (
         <div className="drawer drawer-mobile px-12">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
@@ -13,15 +19,24 @@ const DashBoard = () => {
             <div className="drawer-side">
                 <label for="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                    {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashBoard'>My Orders</Link></li>
-                    <li><Link to='/dashBoard/review'>Add a Review</Link></li>
+                    < li > <Link to='/dashBoard'>My Orders</Link></li>
+
+                    {
+                        user && <li><Link to='/dashBoard/review'>Add a Review</Link></li>
+                    }
                     <li><Link to='/dashBoard/myProfile'>My Profile</Link></li>
-                    <li><Link to='/dashBoard/manageUsers'>Manage Users</Link></li>
+                    {
+                        admin &&
+                        <><li><Link to='/dashBoard/manageAllOrder'>Manage All Orders</Link></li>
+                            <li><Link to='/dashBoard/addProduct'>Add a Product</Link></li>
+                            <li><Link to='/dashBoard/manageProduct'>Manage Products</Link></li>
+                            <li><Link to='/dashBoard/manageUsers'>Make Admin</Link></li>
+                        </>
+                    }
                 </ul>
 
             </div>
-        </div>
+        </div >
     );
 };
 
