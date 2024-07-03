ff27cc910b3840591b6f8ede1cf327ef4298fe6d
Waheedul Islam <waheeduli81@gmail.com>
2024-07-04 03:05:33 +0600

    Return-project

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import AboutUs from '../LoginRegister/AboutUs';
-import Review from '../Review/Review';
-import ExtraSection from './ExtraSection/ExtraSection';
-import BusinessSummery from './Home/BussnessSummery/BusinessSummery';
-import Carousel from './Home/Carsoule/Carousel';
-import Tools from './Home/Tools/Tools';
+import React from "react";
+import AboutUs from "../LoginRegister/AboutUs";
+import Review from "../Review/Review";
+import ExtraSection from "./ExtraSection/ExtraSection";
+import BusinessSummery from "./Home/BussnessSummery/BusinessSummery";
+import Carousel from "./Home/Carsoule/Carousel";
+import Tools from "./Home/Tools/Tools";
 
 const Home = () => {
-    return (
-        <div >
-            <Carousel></Carousel>
-            <div className='mx-w-7xl mx-auto px-12'>
-                <Tools></Tools>
-            </div>
-            <div className='mx-w-7xl mx-auto px-12'>
-                <AboutUs></AboutUs>
-            </div >
-            <BusinessSummery></BusinessSummery>
-            <div className='mx-w-7xl mx-auto px-12'>
-                <ExtraSection></ExtraSection>
-                <Review></Review>
-
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      {/* Return project  */}
+      <Carousel></Carousel>
+      <div className="mx-w-7xl mx-auto px-12">
+        <Tools></Tools>
+      </div>
+      <div className="mx-w-7xl mx-auto px-12">
+        <AboutUs></AboutUs>
+      </div>
+      <BusinessSummery></BusinessSummery>
+      <div className="mx-w-7xl mx-auto px-12">
+        <ExtraSection></ExtraSection>
+        <Review></Review>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
